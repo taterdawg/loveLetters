5e984b2e57009d27acc7c60b4b905f685a3aa811
Anonymous <nathanfarmer@Nathans-Air-2.lan>
2022-04-14 16:06:30 -0400

    Updates

---
+++ precache-manifest.1ded650508c258605c67cb752b97af5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0c4a0288abd72f4fb5f",
+    "url": "/loveletters/static/css/main.aaee5eb2.chunk.css"
+  },
+  {
+    "revision": "f0c4a0288abd72f4fb5f",
+    "url": "/loveletters/static/js/main.f0c4a028.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/loveletters/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "7f2fff4c0ce1c5df8443",
+    "url": "/loveletters/static/js/2.7f2fff4c.chunk.js"
+  },
+  {
+    "revision": "fe7999c4742c7e18e8fc",
+    "url": "/loveletters/static/js/runtime~main.fe7999c4.js"
+  },
+  {
+    "revision": "c99302eed051f985bf14c9c3fd074474",
+    "url": "/loveletters/index.html"
+  }
+];

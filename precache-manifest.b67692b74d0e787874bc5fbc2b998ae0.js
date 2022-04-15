926cf76e9fe4857a131856455bd399d939201773
Anonymous <nathanfarmer@Nathans-Air-2.lan>
2022-04-15 16:44:17 -0400

    Updates

---
+++ precache-manifest.b67692b74d0e787874bc5fbc2b998ae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d802247094de60e96f5",
+    "url": "./static/css/main.b68c0133.chunk.css"
+  },
+  {
+    "revision": "0d802247094de60e96f5",
+    "url": "./static/js/main.0d802247.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "./static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "7f2fff4c0ce1c5df8443",
+    "url": "./static/js/2.7f2fff4c.chunk.js"
+  },
+  {
+    "revision": "871b0a9295e2f7cca613",
+    "url": "./static/js/runtime~main.871b0a92.js"
+  },
+  {
+    "revision": "e79ccc914d01b2180ed20ce4324e2aac",
+    "url": "./index.html"
+  }
+];
